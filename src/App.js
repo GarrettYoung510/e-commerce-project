d792a91758726e4e6a1a136586f7e4bcb5da79ff
birna <briandam26@yahoo.com>
2019-08-21 17:37:31 -0700

    Worked on Home, ProductCard, and Loader components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 // Add Navbar to all pages
-import Navbar from'./components/container/Navbar/Navbar';
+import Navbar from './components/container/Navbar/Navbar';
+import Home from'./components/container/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Home />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
