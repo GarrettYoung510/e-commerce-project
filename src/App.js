c6c8b3e4d69ed624b095d33f2898cedba50a1abb
birna <briandam26@yahoo.com>
2019-08-21 17:03:15 -0700

    Initial building of React components, added Navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Add Navbar to all pages
+import Navbar from'./components/container/Navbar/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
