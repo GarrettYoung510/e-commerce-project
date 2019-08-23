70476ea135b2c51da3b63c591eec497808037578
birna <briandam26@yahoo.com>
2019-08-23 12:17:06 -0700

    added login and logout functions to Navbar

--- src/components/container/Navbar/Navbar.js
+++ src/components/container/Navbar/Navbar.js
@@ -4,11 +4,31 @@ import './Navbar.css';
 // import withRouter so, you can access this.props.history
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+// Import axios to be able to logout the user
+import axios from 'axios';
 
 export class Navbar extends Component {
     // Define linkFunc so it redirects to specified route provided
     linkFunc(path) {
         this.props.history.push(path);
+    }
+    // Logout function that will logout hte user from the initialState
+    logout = () => {
+        // Pass a empty since you are just logging out the user 
+        axios.post('/api/logout', {})
+            .then(res => {
+                // The will logout a user
+                alert(res.data.message);
+                // Reload the browser using this.props.history.go();
+                this.props.history.go();
+            }).catch(err => console.log('Logout Axios Error-------', err));
+    }
+    login = () => {
+        // Define your encodedURi since your are dealing with oAuth, so it can be secure, and oAuth and decode it
+        // Have your location origin, with your auth0 proxy which will be auth/calllback
+        const redirectURI = encodeURIComponent(`${window.location.origin}/auth/callback`);
+        // Redirect the user to your Auth0 domain, with the correct query parameters to retrieve code from auth0
+        window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/login?client=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectURI}`;
     }
     render() {
         console.log(this.props.user);
