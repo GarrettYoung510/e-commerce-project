078bf9f23766f1cf0d7d4e9527eb367bcc48bd36
birna <briandam26@yahoo.com>
2019-08-23 15:45:32 -0700

    Restructuring wireframe for scalability purposes; core and user components added. More restructuring incoming

--- src/App.js
+++ src/App.js
@@ -1,43 +1,5 @@
-import React, { Component } from 'react';
-// Add Navbar to all pages
-import Navbar from './components/container/Navbar/Navbar';
-// import Home from'./components/container/Home/Home';
-import Routes from './Routes';
-// Import withROuter since using redux and want app to have access to this.props.history
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-// Import actions that will be implemented in your reducer
-import { login, logout } from './redux/reducer';
-// import axios to get your user session from your backend
-import axios from 'axios';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  // Get the session in componentDidMount, and if it has data set intialState user to it
-  componentDidMount() {
-    axios.get('/api/user-data')
-    .then(res => {
-      const { dispatch } = this.props;
-      if (res.data.user) {
-        //Dispatch the login function with the user data. 
-        dispatch(login(res.data.user));
-        //Else logout the user from the intialState.
-      } else {
-        //Dispatch the logout the user by default if there is no data in session.
-        dispatch(logout());
-      }
-    })
-  }
+const App = () => <div>Hello from React</div>;
 
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        {Routes}
-      </div>
-    );
-  }
-}
-
-// Then connect app to reducer
-export default withRouter(connect()(App));
+export default App;
