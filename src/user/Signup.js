b106cc81c52eaa15bb0a713c3e29565423e51c5d
birna <briandam26@yahoo.com>
2019-08-23 16:57:31 -0700

    set up env & config; worked on Signup component

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
-const Signup = () => (
-    <Layout title="Signup" description="Signup to Node React E-commerce App">
-        ...
-    </Layout>
-);
+const Signup = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    });
+    const { name, email, password } = values;
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    // POST new user information into DB
+    const signup = user => {
+        fetch(`${API}/signup`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    // Clicking submit registers the user's information
+    const clickSubmit = event => {
+        event.preventDefault();
+        signup({ name, email, password });
+    };
+
+    // Take in user's name, email, and password via form
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    onChange={handleChange("name")}
+                    type="text"
+                    className="form-control"
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input
+                    onChange={handleChange("email")}
+                    type="email"
+                    className="form-control"
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input
+                    onChange={handleChange("password")}
+                    type="password"
+                    className="form-control"
+                />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    );
+
+    return (
+        <Layout
+            title="Signup"
+            description="Signup to Node React E-commerce App"
+            className="container col-md-8 offset-md-2"
+        >
+            {signUpForm()}
+            {JSON.stringify(values)}
+        </Layout>
+    );
+};
 
 export default Signup;
