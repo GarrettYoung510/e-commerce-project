7ef0bba473a10422649be70cc7e8d33730db9f12
Garrett Young <garrettyoung510@gmail.com>
2019-08-23 18:01:16 -0700

    added admin middleware and admin only access

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const expressValidator = require("express-validator");
 // allows us to use .env
 require("dotenv").config();
 // import routes
+const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
 // app
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 
 // routes middleware
+app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 
 const port = process.env.PORT || 3000;
