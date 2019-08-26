e3267ec5855387e9683ce3a7b49a3a6adc127b5e
birna <briandam26@yahoo.com>
2019-08-26 16:58:17 -0700

    Add category componenty with admin access to create

--- ecommerce-back/controllers/auth.js
+++ ecommerce-back/controllers/auth.js
@@ -1,10 +1,10 @@
 const User = require("../models/user");
-const jwt = require("jsonwebtoken"); //to generate signed token
+const jwt = require("jsonwebtoken"); // to generate signed token
 const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
-  //   console.log("req.body", req.body);
+  // console.log("req.body", req.body);
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
@@ -26,30 +26,29 @@ exports.signin = (req, res) => {
   User.findOne({ email }, (err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        error: "User with that email does not exist. Please sign up"
+        error: "User with that email does not exist. Please signup"
       });
     }
-    // if user is found make sure email and password match
+    // if user is found make sure the email and password match
     // create authenticate method in user model
     if (!user.authenticate(password)) {
       return res.status(401).json({
-        error: "Email and password do not match."
+        error: "Email and password dont match"
       });
     }
-    // generate a signed token with secret
+    // generate a signed token with user id and secret
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
     // persist the token as 't' in cookie with expiry date
     res.cookie("t", token, { expire: new Date() + 9999 });
-    // return res with user and token to front end client
+    // return response with user and token to frontend client
     const { _id, name, email, role } = user;
     return res.json({ token, user: { _id, email, name, role } });
   });
 };
 
-// clear the cookie with name of t
 exports.signout = (req, res) => {
   res.clearCookie("t");
-  res.json({ message: "Signout successful." });
+  res.json({ message: "Signout success" });
 };
 
 exports.requireSignin = expressJwt({
@@ -68,9 +67,10 @@ exports.isAuth = (req, res, next) => {
 };
 
 exports.isAdmin = (req, res, next) => {
-  if(req.profile.role === 0) {
-    return res.status(403).json ({
-      error: "Admin resource! Access denied."
-    })
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "Admin resourse! Access denied"
+    });
   }
-}
+  next();
+};
