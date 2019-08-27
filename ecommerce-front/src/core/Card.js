75ce8809ac68cdd8c5e7c3a0e9d5ea06d90d41fe
birna <briandam26@yahoo.com>
2019-08-26 21:20:38 -0700

    Added product's image to the cards

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -7,12 +7,16 @@ const Card = ({ product }) => {
             <div className="card">
                 <div className="card-header">{product.name}</div>
                 <div className="card-body">
+                    {/* Add product's image */}
+                    <ShowImage item={product} url="product" />
                     <p>{product.description}</p>
                     <p>${product.price}</p>
+                    {/* Link to be redirected to view more information on product */}
                     <Link to="/">
                         <button className="btn btn-outline-primary mt-2 mb-2">
                             View Product
                         </button>
+                        {/* Link to be redirected to add item(s) to your cart */}
                     </Link>
                     <button className="btn btn-outline-warning mt-2 mb-2">
                         Add to Cart
