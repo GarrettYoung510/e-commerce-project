8287864731cc02527a8ac1bbd97a4db9032e5a4f
birna <briandam26@yahoo.com>
2019-08-26 18:56:51 -0700

    worked on GET products to list onto the page as cards

--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
 
-const Home = () => (
-    <Layout title="Home Page" description="Node React E-commerce App">
-        ...
-    </Layout>
-);
+const Home = () => {
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    // eslint-disable-next-line
+    const [error, setError] = useState(false);
+
+    // Sorting products by most sold first
+    const loadProductsBySell = () => {
+        getProducts("sold").then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsBySell(data);
+            }
+        });
+    };
+
+    // Sorting products by newest entry first
+    const loadProductsByArrival = () => {
+        getProducts("createdAt").then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsByArrival(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
+
+    return (
+        <Layout
+            title="Home Page"
+            description="Node React E-commerce App"
+            className="container-fluid"
+        >
+            <h2 className="mb-4">New Arrivals</h2>
+            <div className="row">
+                {productsByArrival.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
+
+            <h2 className="mb-4">Best Sellers</h2>
+            <div className="row">
+                {productsBySell.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
+        </Layout>
+    );
+};
 
 export default Home;
