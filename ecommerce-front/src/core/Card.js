a23ecf33dcac65485544fba2e4b5a8b5bc94de91
Garrett Young <garrettyoung510@gmail.com>
2019-08-27 00:14:35 -0700

    filters by category, filters by price range but radio button selection, pass products to card component, updated length of description in card

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ShowImage from "./ShowImage";
+import ShowImage from "./showImage";
 
 const Card = ({ product }) => {
-    return (
-        <div className="col-4 mb-3">
-            <div className="card">
-                <div className="card-header">{product.name}</div>
-                <div className="card-body">
-                    {/* Add product's image */}
-                    <ShowImage item={product} url="product" />
-                    <p>{product.description}</p>
-                    <p>${product.price}</p>
-                    {/* Link to be redirected to view more information on product */}
-                    <Link to="/">
-                        <button className="btn btn-outline-primary mt-2 mb-2">
-                            View Product
-                        </button>
-                        {/* Link to be redirected to add item(s) to your cart */}
-                    </Link>
-                    <button className="btn btn-outline-warning mt-2 mb-2">
-                        Add to Cart
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="col-4 mb-3">
+      <div className="card">
+        <div className="card-header">{product.name}</div>
+        <div className="card-body">
+          {/* Add product's image */}
+          <ShowImage item={product} url="product" />
+          {/* control length of description */}
+          <p>{product.description.substring(0, 100)}</p>
+          <p>${product.price}</p>
+          {/* Link to be redirected to view more information on product */}
+          <Link to="/">
+            <button className="btn btn-outline-primary mt-2 mb-2">
+              View Product
+            </button>
+            {/* Link to be redirected to add item(s) to your cart */}
+          </Link>
+          <button className="btn btn-outline-warning mt-2 mb-2">
+            Add to Cart
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- ecommerce-front/src/core/apiCore.js
+++ ecommerce-front/src/core/apiCore.js
@@ -2,22 +2,44 @@ import { API } from "../config";
 
 // GET request for Products
 export const getProducts = sortBy => {
-    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
-        method: "GET"
+  return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
 
 // GET request for Categories
 export const getCategories = () => {
-    return fetch(`${API}/categories`, {
-        method: "GET"
+  return fetch(`${API}/categories`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
+};
+
+export const getFilteredProducts = (skip, limit, filters = {}) => {
+  const data = {
+    limit,
+    skip,
+    filters
+  };
+  return fetch(`${API}/products/by/search`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
