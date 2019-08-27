7ba22ea7f708fc047a624661d83948abbd2145b7
Garrett Young <garrettyoung510@gmail.com>
2019-08-27 12:24:43 -0700

    single product page with related products updating now

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -1,27 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShowImage from "./ShowImage";
+import moment from "moment";
 
-const Card = ({ product }) => {
-  return (
-    <div className="card">
-      <div className="card-header">{product.name}</div>
-      <div className="card-body">
-        {/* Add product's image */}
-        <ShowImage item={product} url="product" />
-        {/* control length of description */}
-        <p>{product.description.substring(0, 100)}</p>
-        <p>${product.price}</p>
-        {/* Link to be redirected to view more information on product */}
-        <Link to={`/product/${product._id}`}>
+const Card = ({ product, showViewProductButton = true }) => {
+  const showViewButton = showViewProductButton => {
+    return (
+      showViewProductButton && (
+        <Link to={`/product/${product._id}`} className="mr-2">
           <button className="btn btn-outline-primary mt-2 mb-2">
             View Product
           </button>
-          {/* Link to be redirected to add item(s) to your cart */}
         </Link>
-        <button className="btn btn-outline-warning mt-2 mb-2">
-          Add to Cart
-        </button>
+      )
+    );
+  };
+
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-warning mt-2 mb-2">Add to cart</button>
+    );
+  };
+
+  const showStock = quantity => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock</span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of Stock</span>
+    );
+  };
+
+  return (
+    <div className="card">
+      <div className="card-header name">{product.name}</div>
+      <div className="card-body">
+        <ShowImage item={product} url="product" />
+        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+        <p className="black-10">${product.price}</p>
+        <p className="black-9">
+          Category: {product.category && product.category.name}
+        </p>
+        <p className="black-8">
+          Added on {moment(product.createdAt).fromNow()}
+        </p>
+
+        {showStock(product.quantity)}
+        <br />
+        {showViewButton(showViewProductButton)}
+
+        {showAddToCartButton()}
       </div>
     </div>
   );
--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -5,60 +5,65 @@ import Card from "./Card";
 import Search from "./Search";
 
 const Home = () => {
-    const [productsBySell, setProductsBySell] = useState([]);
-    const [productsByArrival, setProductsByArrival] = useState([]);
-    // eslint-disable-next-line
-    const [error, setError] = useState(false);
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  // eslint-disable-next-line
+  const [error, setError] = useState(false);
 
-    // Sorting products by most sold first
-    const loadProductsBySell = () => {
-        getProducts("sold").then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setProductsBySell(data);
-            }
-        });
-    };
+  // Sorting products by most sold first
+  const loadProductsBySell = () => {
+    getProducts("sold").then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
 
-    // Sorting products by newest entry first
-    const loadProductsByArrival = () => {
-        getProducts("createdAt").then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setProductsByArrival(data);
-            }
-        });
-    };
+  // Sorting products by newest entry first
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
 
-    useEffect(() => {
-        loadProductsByArrival();
-        loadProductsBySell();
-    }, []);
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
 
-    return (
-        <Layout
-            title="Home Page"
-            description="Node React E-commerce App"
-            className="container-fluid"
-        >
-            <Search />
-            <h2 className="mb-4">New Arrivals</h2>
-            <div className="row">
-                {productsByArrival.map((product, i) => (
-                    <Card key={i} product={product} />
-                ))}
-            </div>
+  return (
+    <Layout
+      title="Home Page"
+      description="Node React E-commerce App"
+      className="container-fluid"
+    >
+      <Search />
+      <h2 className="mb-4">New Arrivals</h2>
+      <div className="row">
+        {productsByArrival.map((product, i) => (
+            // using key in div instead of card to take effect on the div
+          <div key={i} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
+        ))}
+      </div>
 
-            <h2 className="mb-4">Best Sellers</h2>
-            <div className="row">
-                {productsBySell.map((product, i) => (
-                    <Card key={i} product={product} />
-                ))}
-            </div>
-        </Layout>
-    );
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySell.map((product, i) => (
+          <div key={i} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
