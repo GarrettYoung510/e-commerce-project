62929a40875a55be93a143e6a9f05eda43a55c36
birna <briandam26@yahoo.com>
2019-08-27 12:56:14 -0700

    add to cart functionality with the menu showing the total added

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -1,24 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import moment from "moment";
+import { addItem } from "./cartHelpers";
 
 const Card = ({ product, showViewProductButton = true }) => {
+  const [redirect, setRedirect] = useState(false);
+
   const showViewButton = showViewProductButton => {
     return (
       showViewProductButton && (
         <Link to={`/product/${product._id}`} className="mr-2">
           <button className="btn btn-outline-primary mt-2 mb-2">
             View Product
-          </button>
+                    </button>
         </Link>
       )
     );
   };
 
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = redirect => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
   const showAddToCartButton = () => {
     return (
-      <button className="btn btn-outline-warning mt-2 mb-2">Add to cart</button>
+      <button
+        onClick={addToCart}
+        className="btn btn-outline-warning mt-2 mb-2"
+      >
+        Add to cart
+            </button>
     );
   };
 
@@ -26,16 +46,19 @@ const Card = ({ product, showViewProductButton = true }) => {
     return quantity > 0 ? (
       <span className="badge badge-primary badge-pill">In Stock</span>
     ) : (
-      <span className="badge badge-primary badge-pill">Out of Stock</span>
-    );
+        <span className="badge badge-primary badge-pill">Out of Stock</span>
+      );
   };
 
   return (
     <div className="card">
       <div className="card-header name">{product.name}</div>
       <div className="card-body">
+        {shouldRedirect(redirect)}
         <ShowImage item={product} url="product" />
-        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+        <p className="lead mt-2">
+          {product.description.substring(0, 100)}
+        </p>
         <p className="black-10">${product.price}</p>
         <p className="black-9">
           Category: {product.category && product.category.name}
@@ -46,6 +69,7 @@ const Card = ({ product, showViewProductButton = true }) => {
 
         {showStock(product.quantity)}
         <br />
+
         {showViewButton(showViewProductButton)}
 
         {showAddToCartButton()}
