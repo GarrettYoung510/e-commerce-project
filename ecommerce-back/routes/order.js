05f23e2c222d1527d152e1bf706b44ea61508e09
Garrett Young <garrettyoung510@gmail.com>
2019-08-27 20:19:26 -0700

    can update order status as admin

--- ecommerce-back/routes/order.js
+++ ecommerce-back/routes/order.js
@@ -3,20 +3,36 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const { create, listOrders, getStatusValues, orderById, updateOrderStatus } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
-    "/order/create/:userId",
-    requireSignin,
-    isAuth,
-    addOrderToUserHistory,
-    decreaseQuantity,
-    create
+  "/order/create/:userId",
+  requireSignin,
+  isAuth,
+  addOrderToUserHistory,
+  decreaseQuantity,
+  create
 );
 
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+// put method to update something
+router.put(
+  "/order/:orderId/status/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
 
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
--- ecommerce-front/src/admin/apiAdmin.js
+++ ecommerce-front/src/admin/apiAdmin.js
@@ -2,63 +2,95 @@ import { API } from "../config";
 
 // Request for POST method to create a category
 export const createCategory = (userId, token, category) => {
-    return fetch(`${API}/category/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(category)
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(category)
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 // Request for POST method to create a product
 export const createProduct = (userId, token, product) => {
-    return fetch(`${API}/product/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: product
+  return fetch(`${API}/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 // Request to GET categories from DB to use with product creation
 export const getCategories = () => {
-    return fetch(`${API}/categories`, {
-        method: "GET"
+  return fetch(`${API}/categories`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
 
 // Request to GET list of customer's orders
 export const listOrders = (userId, token) => {
-    return fetch(`${API}/order/list/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        }
+  return fetch(`${API}/order/list/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
+};
+
+// Request to GET list of status values
+export const getStatusValues = (userId, token) => {
+  return fetch(`${API}/order/status-values/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+// Request to PUT list of update order status
+export const updateOrderStatus = (userId, token, orderId, status) => {
+  return fetch(`${API}/order/${orderId}/status/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ status, orderId })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
 };
