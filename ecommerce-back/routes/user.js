9bb0feac025981a914027791abe6014d50d098d8
birna <briandam26@yahoo.com>
2019-08-28 00:20:02 -0700

    User Purchase History backend and frontend

--- ecommerce-back/routes/user.js
+++ ecommerce-back/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { userById, read, update } = require("../controllers/user");
+const { userById, read, update, purchaseHistory } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
@@ -14,6 +14,7 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
 // READ and UPDATE user profile 
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.put("/user/:userId", requireSignin, isAuth, update);
+router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
 
 // anytime there is a userid the router will run the method userById
 router.param("userId", userById);
--- ecommerce-front/src/admin/Orders.js
+++ ecommerce-front/src/admin/Orders.js
@@ -21,10 +21,6 @@ const Orders = () => {
     });
   };
 
-  useEffect(() => {
-    loadOrders();
-  }, []);
-
   const loadStatusValues = () => {
     getStatusValues(user._id, token).then(data => {
       if (data.error) {
@@ -43,7 +39,9 @@ const Orders = () => {
   const showOrdersLength = () => {
     if (orders.length > 0) {
       return (
-        <h1 className="text-danger display-2">Total orders: {orders.length}</h1>
+        <h1 className="text-danger display-2">
+          Total orders: {orders.length}
+        </h1>
       );
     } else {
       return <h1 className="text-danger">No orders</h1>;
@@ -55,18 +53,25 @@ const Orders = () => {
       <div className="input-group-prepend">
         <div className="input-group-text">{key}</div>
       </div>
-      <input type="text" value={value} className="form-control" readOnly />
+      <input
+        type="text"
+        value={value}
+        className="form-control"
+        readOnly
+      />
     </div>
   );
 
   const handleStatusChange = (e, orderId) => {
-    updateOrderStatus(user._id, token, orderId, e.target.value).then(data => {
-      if (data.error) {
-        console.log("Status update failed");
-      } else {
-        loadOrders();
+    updateOrderStatus(user._id, token, orderId, e.target.value).then(
+      data => {
+        if (data.error) {
+          console.log("Status update failed");
+        } else {
+          loadOrders();
+        }
       }
-    });
+    );
   };
 
   const showStatus = o => (
@@ -89,12 +94,15 @@ const Orders = () => {
   return (
     <Layout
       title="Orders"
-      description={`G'day ${user.name}, you can manage all the orders here`}
+      description={`G'day ${
+        user.name
+        }, you can manage all the orders here`}
+      className="container-fluid"
     >
       <div className="row">
         <div className="col-md-8 offset-md-2">
           {showOrdersLength()}
-          {/* map through order and order index */}
+
           {orders.map((o, oIndex) => {
             return (
               <div
@@ -103,18 +111,27 @@ const Orders = () => {
                 style={{ borderBottom: "5px solid indigo" }}
               >
                 <h2 className="mb-5">
-                  <span className="bg-primary">Order ID: {o._id}</span>
+                  <span className="bg-primary">
+                    Order ID: {o._id}
+                  </span>
                 </h2>
 
                 <ul className="list-group mb-2">
-                  <li className="list-group-item">{showStatus(o)}</li>
+                  <li className="list-group-item">
+                    {showStatus(o)}
+                  </li>
                   <li className="list-group-item">
                     Transaction ID: {o.transaction_id}
                   </li>
-                  <li className="list-group-item">Amount: {o.amount}</li>
-                  <li className="list-group-item">Ordered by: {o.user.name}</li>
                   <li className="list-group-item">
-                    Ordered on: {moment(o.createdAt).fromNow()}
+                    Amount: ${o.amount}
+                  </li>
+                  <li className="list-group-item">
+                    Ordered by: {o.user.name}
+                  </li>
+                  <li className="list-group-item">
+                    Ordered on:{" "}
+                    {moment(o.createdAt).fromNow()}
                   </li>
                   <li className="list-group-item">
                     Delivery address: {o.address}
@@ -122,19 +139,23 @@ const Orders = () => {
                 </ul>
 
                 <h3 className="mt-4 mb-4 font-italic">
-                  Total products in the order: {o.products.length}
+                  Total products in the order:{" "}
+                  {o.products.length}
                 </h3>
 
                 {o.products.map((p, pIndex) => (
                   <div
                     className="mb-4"
                     key={pIndex}
-                    style={{ padding: "20px", border: "1px solid indigo" }}
+                    style={{
+                      padding: "20px",
+                      border: "1px solid indigo"
+                    }}
                   >
                     {showInput("Product name", p.name)}
                     {showInput("Product price", p.price)}
                     {showInput("Product total", p.count)}
-                    {showInput("Product id", p._id)}
+                    {showInput("Product Id", p._id)}
                   </div>
                 ))}
               </div>
